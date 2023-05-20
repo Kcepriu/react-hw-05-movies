070246f0487fd2c490eb63532cb651482903ee15
Kcepriu <kcepriu@gmail.com>
2023-05-20 12:57:18 +0300

    add folder build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     remotePatterns: [
       {
